e9aeef71b223795ba13f187af0e1e4db4566c4f9
imen.elhammami@gmail.com <imen.elhammami@gmail.com@1387eab2-4ac0-7acd-d414-4f58f15c2aaa>
2012-08-31 01:03:35 +0000

    CAN0 DRIVER (.h)

---
+++ SW_CEDU01/B1.0/DRIVERS/CAN0_DRV/B1.1/CAN0_DRV.h
@@ -0,0 +1,42 @@
+//############################################################################//
+// HEAD : (c) ARDIA 2010 : " Any copie and re-utilisation of this file without
+//                           the written consent of ARDIA is forbidden "
+//        --------------------------------------------------------------------
+//        Classification :  (-)No   (-)Confident. ARDIA   (-)Confident. Client
+//        --------------------------------------------------------------------
+//        --------------------------------------------------------------------
+//        Module name : CAN0_DRV
+//############################################################################//
+// FILE : CAN0_DRV.H (SOURCE)
+//----------------------------------------------------------------------------//
+// DESC : Module description
+//----------------------------------------------------------------------------//
+// LIST : * List of the functions
+//----------------------------------------------------------------------------//
+// HIST : Version |   Date   | Author | Description
+//----------------------------------------------------------------------------//
+//         01.00  | 11/10/10 |  SGHR  | Creation of this file
+//
+//############################################################################//
+
+//****************************************************************************//
+// INCLIB
+//****************************************************************************//
+
+//****************************************************************************//
+// INCUSER
+//****************************************************************************//
+#include "..\..\..\SYSTEME\REG\mb96348rs.h"
+#include "..\..\..\DRIVERS\CAN0_DRV\B1.0\CAN0_DRV.cfg"
+
+#define HEX      0
+#define DEC      1
+#define CAN_TIMEOUT 0xFFFF    // <<< simple timeout value, if CAN-data can not be send
+
+
+int  CAN0_GetRxBuffer(void);
+void CAN0_init (unsigned long ulBaudRate, unsigned char ucFlag_CAN)int CAN0_ReadMessage(unsigned char buffer, unsigned long *id, unsigned char *memory);
+int CAN0_SendMessage(unsigned char buffer, unsigned long id, unsigned char *memory);
+void CAN0_SetRxBuffer(unsigned char buffer, unsigned long id, unsigned char irq, unsigned long mask);
+void vCAN0_ReceiveInterrupt_ISRr(void);
+void vCAN0_TEST_EXE(void);
